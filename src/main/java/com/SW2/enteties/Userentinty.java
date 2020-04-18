1ccc8cca12d937d8f855ffbd2badeb31d3423ca2
petermalak <36422923+petermalak@users.noreply.github.com>
2020-04-18 10:31:41 +0200

    Change database Schema

--- src/main/java/com/SW2/enteties/Userentinty.java
+++ src/main/java/com/SW2/enteties/Userentinty.java
@@ -1,28 +1,31 @@
 package com.SW2.enteties;
 
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
-import net.bytebuddy.asm.Advice.This;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import java.util.Set;
 
 @Entity
+@Table(name = "user")
 public class Userentinty {
 	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
 	
 	
 	private String name;
-	private String userName;
+	private String username;
 	private String password;
+	@ManyToMany
+    @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+	private Set <RoleEntity> roles;
 	
 	public Userentinty() {}
 	public Userentinty(String name, String userName , String password) {
 		this.name=name;
-		this.userName=userName;
+		this.username=userName;
 		this.password=password;
 	}
 	public String getName() {
@@ -32,10 +35,29 @@ public class Userentinty {
 		return password;
 	}
 	public String getUserName() {
-		return userName;
+		return username;
 	}
 	public long getId() {
 		return id;
 	}
-	
+	@JsonBackReference
+	public Set < RoleEntity > getRoles() {
+        return roles;
+    }
+	public void setRoles(Set <RoleEntity> roles) {
+		this.roles=roles;
+	}
+	public void setId(Long id) {
+        this.id = id;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public void setName(String name) {
+    	this.name=name;
+    }
 }
